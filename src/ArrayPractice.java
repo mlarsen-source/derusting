0e9c56045dcdc021080f6c09bc5ae6f2a70c96d9
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-01-07 21:57:22 -0800

    corrected array practice

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -7,10 +7,10 @@ public class ArrayPractice
 
     // Set the value of the array at each index to be a different String
     // It's OK to do this one-by-one
-    newArray[1] = "string1";
-    newArray[2] = "string2";
-    newArray[3] = "string3";
-    newArray[4] = "string4";
+    newArray[0] = "apple";
+    newArray[1] = "banana";
+    newArray[2] = "strawberry";
+    newArray[3] = "pear";
 
     // Get the value of the array at index 2
 
@@ -28,9 +28,9 @@ public class ArrayPractice
     }
 
     // Iterate over the array using a for-each loop and print out each item
-    for(String word: newArray)
+    for(String fruit: newArray)
     {
-      System.out.println(word);
+      System.out.println(fruit);
     }
     
     /*
